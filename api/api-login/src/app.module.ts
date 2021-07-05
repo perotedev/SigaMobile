ac20e9a94022ecad7f33126ffffe8d331d0abf85
rodrigoperote <agenda@rodrigodev.online>
2021-07-04 23:12:58 -0400

    Corrigido problemas com .gitignore e adicionado base backend

---
+++ api/api-login/src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [
+    MongooseModule.forRoot('mongodb://root:example@mongo:27017/devtest?authSource=admin')
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
