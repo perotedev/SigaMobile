ee5c57293852b748a3502076889ddda6326a3697
Rodrigo Perote <agenda@rodrigodev.online>
2021-07-13 09:41:59 -0400

    Corrigido studantId do usuario teste joaomelo

--- database-scripts/fake-users.js
+++ database-scripts/fake-users.js
@@ -3,7 +3,7 @@ db.users.insertMany([
         _id: ObjectId("60d8ad29967c085268fb5179"),
         username: "joaomelo",
         passwordHash: "$2b$10$JAsg1Dt2UXeoREWc3ltIq.FsCWTaBjJYZRR3OlvjoLkboW2zBc2Nu",  //password
-        studantId: ObjectId("60d8ad29967c095068fb5172"),
+        studantId: ObjectId("60d8ad29967c085268fb5184"),
         createdAt: ISODate("2021-06-29T00:04:01.665Z"),
         updatedAt: ISODate("2021-06-29T00:04:01.665Z")
     }
